b79e049bfaef0db19fcb15887144368dbaee1f64
jnorgini <jnorgini@gmail.com>
2026-09-09 17:42:04 -0300

    feat: sqs template

--- src/main/java/com/norgini/sqs/SqsApplication.java
+++ src/main/java/com/norgini/sqs/SqsApplication.java
@@ -1,13 +1,29 @@
 package com.norgini.sqs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.norgini.sqs.consumer.MyMessage;
+
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+
 @SpringBootApplication
-public class SqsApplication {
+public class SqsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SqsApplication.class, args);
+	}
+	
+	@Autowired
+	private SqsTemplate sqsTemplate;
+
+	@Override
+	public void run(String... args) throws Exception {
+		var SQS = "https://localhost.localstack.cloud:4566/000000000000/minha-fila";
+		sqsTemplate.send(SQS, new MyMessage("meu valor de start"));
+		
 	}
 
 }
